018af945c6951f593aceffedc6ef2f831f0b95ec
Rinz <rinceee@gmail.com>
2012-07-15 00:11:25 +0200

    replace getJSON to .ajax

--- bookmarklet.js
+++ bookmarklet.js
@@ -102,19 +102,22 @@ script.onload = script.onreadystatechange = function(){
 							var callback = function(url){
 								
 								AJAX_REQUEST++;
-							
-								jQuery.getJSON("http://urls.api.twitter.com/1/urls/count.json?url=" + url + "&callback=?", function(data){
+								
+								jQuery.ajax({
+									url: "http://urls.api.twitter.com/1/urls/count.json?url=" + url + "&callback=?",
+									crossDomain: true,
+									dataType: "json",
+									success: function(data) {
+										jQuery(e).attr("count", data.count || 0);
+										jQuery(".entry-title", e).html("[" + "<img style=\"margin-bottom: -3px\" src=\"https://twitter.com/favicons/favicon.ico\">" + (data.count || 0) + "] " + jQuery(".entry-title", e).html());
 									
-									jQuery(e).attr("count", data.count || 0);
-									jQuery(".entry-title", e).html("[" + "<img style=\"margin-bottom: -3px\" src=\"https://twitter.com/favicons/favicon.ico\">" + (data.count || 0) + "] " + jQuery(".entry-title", e).html());
-									
-									jQuery(".collapsed", e).css("background-color", getColorForAmount(data.count));
-									
-									AJAX_REQUEST--;
-									if(AJAX_REQUEST == 0){
-										propagetElement("count");
+										jQuery(".collapsed", e).css("background-color", getColorForAmount(data.count));
+										
+										AJAX_REQUEST--;
+										if(AJAX_REQUEST == 0){
+											propagetElement("count");
+										}
 									}
-									
 								});
 							}
 							
